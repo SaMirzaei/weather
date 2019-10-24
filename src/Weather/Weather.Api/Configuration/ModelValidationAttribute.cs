37168816cd8d6a0383079f88673858a00dd138eb
Saeid <saeidmirzaei@live.com>
2019-10-24 11:04:20 +0330

    Added ModelValidation

---
+++ src/Weather/Weather.Api/Configuration/ModelValidationAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Weather.Api.Configuration
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    public class ModelValidationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState); // returns 400 with error
+            }
+        }
+    }
+}
